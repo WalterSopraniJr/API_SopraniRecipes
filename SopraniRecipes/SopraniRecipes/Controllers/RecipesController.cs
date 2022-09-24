1e0b07ac8d8fe5664afae54c6bffabbcb93f4c94
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-09-24 20:44:09 -0300

    Início da API seguindo as instruções do Microsoft Learn.

---
+++ SopraniRecipes/SopraniRecipes/Controllers/RecipesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SopraniRecipes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+
+        public string[] GetDishes()
+        {
+            string[] dishes = { "Picanha ao Forno", "Feijoada", "Sukiake" };
+            return dishes;
+        }
+    }
+}
